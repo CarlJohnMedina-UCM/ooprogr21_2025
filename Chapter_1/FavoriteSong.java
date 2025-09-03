959ae6bddb3ec3bf24cdf472690a75184a49d03e
CarlJohnMedina-UCM <carljohnm19@gmail.com>
2025-09-03 13:49:56 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,17 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        String[] songLines = {
+            "When we're out in a crowd",
+            "Laughin' loud and nobody knows why",
+            "When we're lost at a club",
+            "Gettin' drunk and you give me that smile",
+            "Goin' home in the back of a car",
+            "And your hand touches mine",
+            
+        };
+
+        for (int i = 0; i < songLines.length; i++) {
+            System.out.println(songLines[i]);
+        }
+    }
+}
